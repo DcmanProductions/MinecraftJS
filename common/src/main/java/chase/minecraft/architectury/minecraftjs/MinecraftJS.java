6b9b68f32e083a9bf452aca7d5a75618930f8164
Drew Chase <dcmanproductions@gmail.com>
2023-08-22 12:08:00 -0400

    added ScriptLoader

--- common/src/main/java/chase/minecraft/architectury/minecraftjs/MinecraftJS.java
+++ common/src/main/java/chase/minecraft/architectury/minecraftjs/MinecraftJS.java
@@ -8,11 +8,15 @@ public class MinecraftJS
 {
 	public static final String MOD_ID = "minecraftjs";
 	public static final Logger log = LogManager.getLogger(MOD_ID);
-
-	public static void init() {
-		log.info("Starting {}", MOD_ID);
+	
+	public static void init()
+	{
+		ScriptLoader.Instance.LoadScripts();
 	}
-	public static ResourceLocation getModResource(String name){
+	
+	public static ResourceLocation getModResource(String name)
+	{
 		return new ResourceLocation(MOD_ID, name);
 	}
+	
 }
